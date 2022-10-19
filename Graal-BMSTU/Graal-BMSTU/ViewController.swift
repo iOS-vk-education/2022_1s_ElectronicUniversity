86b34bde6f37afc138af5c3b78181d63c089b609
Artem Tikhonenko <tinartem@gmail.com>
2022-10-19 19:43:28 +0300

    init

---
+++ Graal-BMSTU/Graal-BMSTU/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Graal-BMSTU
+//
+//  Created by Артём on 19.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
