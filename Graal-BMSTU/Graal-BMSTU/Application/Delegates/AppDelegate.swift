9695975332ceb0666129f7364b23bd6abbe052e0
kochetova <kochetova>
2022-12-08 13:25:39 +0300

    можно не смотреть, первая версия расписания, пока сломана

--- Graal-BMSTU/Graal-BMSTU/Application/Delegates/AppDelegate.swift
+++ Graal-BMSTU/Graal-BMSTU/Application/Delegates/AppDelegate.swift
@@ -13,7 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let navigationController = UINavigationController.init(rootViewController:
+            ViewController())
+        
+        self.window = UIWindow.init(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
         return true
     }
 
