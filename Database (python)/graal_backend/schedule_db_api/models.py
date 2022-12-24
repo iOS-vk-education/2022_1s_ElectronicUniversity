674749f6ec9711ddac3c66fa9568ec4681c0c377
Artem Tikhonenko <tinartem@gmail.com>
2022-12-25 01:47:29 +0300

    backend models change

--- Database (python)/graal_backend/schedule_db_api/models.py
+++ Database (python)/graal_backend/schedule_db_api/models.py
@@ -5,9 +5,17 @@ from django.db import models
 
 
 class StudyStream(models.Model):
+
+    class StudyLevel(models.TextChoices):
+        BACH = "BACHELOR"
+        MAST = "MASTER"
+        POSTGR = "POSTGRADUATE"
+
     semester = models.IntegerField()
-    specialty = models.CharField(max_length=255)
     faculty = models.CharField(max_length=255)
+    study_level = models.CharField(max_length=12, choices=StudyLevel.choices)
+    semester_start = models.DateField()
+    semester_end = models.DateField()
 
 
 class Group(models.Model):
@@ -22,27 +30,28 @@ class Subject(models.Model):
 
 class Place(models.Model):
 
-    class BMSTUBuilding(models.TextChoices):
-        GZ = "GZ"
-        ULK = "ULK"
-        LT = "LT"
-        E = "E"
-        SM = "SM"
-        MT = "MT"
-        MF = "MF"
-        FV = "FV"
-
-    def get_building(self) -> BMSTUBuilding:
-        return self.BMSTUBuilding(self.building)
-
+    # class BMSTUBuilding(models.TextChoices):
+    #     GZ = "GZ"
+    #     ULK = "ULK"
+    #     LT = "LT"
+    #     E = "E"
+    #     SM = "SM"
+    #     MT = "MT"
+    #     MF = "MF"
+    #     FV = "FV"
+    #
+    # def get_building(self) -> BMSTUBuilding:
+    #     return self.BMSTUBuilding(self.building)
     name = models.CharField(max_length=255)
-    building = models.CharField(max_length=3, choices=BMSTUBuilding.choices)
+    is_generic = models.BooleanField()  # если это что-то типа "каф. ФН12"
+    # building = models.CharField(max_length=3, choices=BMSTUBuilding.choices)
 
 
 class Teacher(models.Model):
-    familyName = models.CharField(max_length=255)
-    forename = models.CharField(max_length=255)  # именно имя
-    fatherName = models.CharField(max_length=255)
+    # familyName = models.CharField(max_length=255)
+    # forename = models.CharField(max_length=255)  # именно имя
+    # fatherName = models.CharField(max_length=255)
+    display_name = models.CharField(max_length=255)
     study_streams = models.ManyToManyField("StudyStream")
 
 
